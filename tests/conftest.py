29e775fa08f62d57e546a7cd9a59207deb86b9ee
Felddy <markf@geekpad.com>
2020-05-13 21:32:35 -0400

    Resolve lineage PR conflicts.

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,7 +5,6 @@ https://docs.pytest.org/en/latest/writing_plugins.html#conftest-py-plugins
 # Third-Party Libraries
 import pytest
 
-<<<<<<< HEAD
 
 @pytest.fixture(scope="session")
 def gophish_container(dockerc):
@@ -26,27 +25,6 @@ def mailhog_container(dockerc):
     """Return the mailhog container from the docker composition."""
     # find the container by name even if it is stopped already
     return dockerc.containers(service_names=["mailhog"], stopped=True)[0]
-=======
-MAIN_SERVICE_NAME = "example"
-VERSION_SERVICE_NAME = f"{MAIN_SERVICE_NAME}-version"
-
-
-@pytest.fixture(scope="session")
-def main_container(dockerc):
-    """Return the main container from the docker composition."""
-    # find the container by name even if it is stopped already
-    return dockerc.containers(service_names=[MAIN_SERVICE_NAME], stopped=True)[0]
-
-
-@pytest.fixture(scope="session")
-def version_container(dockerc):
-    """Return the version container from the docker composition.
-
-    The version container should just output the version of its underlying contents.
-    """
-    # find the container by name even if it is stopped already
-    return dockerc.containers(service_names=[VERSION_SERVICE_NAME], stopped=True)[0]
->>>>>>> 4de6b59dd041229073cb15571ec5a1c005f6cad6
 
 
 def pytest_addoption(parser):
--- tests/container_test.py
+++ tests/container_test.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env pytest -vs
-<<<<<<< HEAD
 """Tests for Docker composition."""
 
 # Standard Python Libraries
@@ -10,32 +9,12 @@ READY_MESSAGES = {
     "postfix": "daemon started",
     "mailhog": "Creating API v2 with WebPath",
 }
-=======
-"""Tests for example container."""
-
-# Standard Python Libraries
-import os
-import time
-
-# Third-Party Libraries
-import pytest
-
-ENV_VAR = "ECHO_MESSAGE"
-ENV_VAR_VAL = "Hello World from docker-compose!"
-READY_MESSAGE = "This is a debug message"
-SECRET_QUOTE = (
-    "There are no secrets better kept than the secrets everybody guesses."  # nosec
-)
-RELEASE_TAG = os.getenv("RELEASE_TAG")
-VERSION_FILE = "src/version.txt"
->>>>>>> 4de6b59dd041229073cb15571ec5a1c005f6cad6
 
 
 def test_container_count(dockerc):
     """Verify the test composition and container."""
     # stopped parameter allows non-running containers in results
     assert (
-<<<<<<< HEAD
         len(dockerc.containers(stopped=True)) == 3
     ), "Wrong number of containers were started."
 
@@ -46,23 +25,11 @@ def test_wait_for_ready_gophish(gophish_container):
     ready_message = READY_MESSAGES["gophish"]
     for i in range(TIMEOUT):
         if ready_message in gophish_container.logs().decode("utf-8"):
-=======
-        len(dockerc.containers(stopped=True)) == 2
-    ), "Wrong number of containers were started."
-
-
-def test_wait_for_ready(main_container):
-    """Wait for container to be ready."""
-    TIMEOUT = 10
-    for i in range(TIMEOUT):
-        if READY_MESSAGE in main_container.logs().decode("utf-8"):
->>>>>>> 4de6b59dd041229073cb15571ec5a1c005f6cad6
             break
         time.sleep(1)
     else:
         raise Exception(
             f"Container does not seem ready.  "
-<<<<<<< HEAD
             f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
         )
 
@@ -95,9 +62,6 @@ def test_wait_for_ready_mailhog(mailhog_container):
             f"Container does not seem ready.  "
             f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
         )
-=======
-            f'Expected "{READY_MESSAGE}" in the log within {TIMEOUT} seconds.'
-        )
 
 
 def test_wait_for_exits(main_container, version_container):
@@ -106,49 +70,3 @@ def test_wait_for_exits(main_container, version_container):
     assert (
         version_container.wait() == 0
     ), "Container service (version) did not exit cleanly"
-
-
-def test_output(main_container):
-    """Verify the container had the correct output."""
-    main_container.wait()  # make sure container exited if running test isolated
-    log_output = main_container.logs().decode("utf-8")
-    assert SECRET_QUOTE in log_output, "Secret not found in log output."
-
-
-@pytest.mark.skipif(
-    RELEASE_TAG in [None, ""], reason="this is not a release (RELEASE_TAG not set)"
-)
-def test_release_version():
-    """Verify that release tag version agrees with the module version."""
-    pkg_vars = {}
-    with open(VERSION_FILE) as f:
-        exec(f.read(), pkg_vars)  # nosec
-    project_version = pkg_vars["__version__"]
-    assert (
-        RELEASE_TAG == f"v{project_version}"
-    ), "RELEASE_TAG does not match the project version"
-
-
-def test_log_version(version_container):
-    """Verify the container outputs the correct version to the logs."""
-    version_container.wait()  # make sure container exited if running test isolated
-    log_output = version_container.logs().decode("utf-8").strip()
-    pkg_vars = {}
-    with open(VERSION_FILE) as f:
-        exec(f.read(), pkg_vars)  # nosec
-    project_version = pkg_vars["__version__"]
-    assert (
-        log_output == project_version
-    ), f"Container version output to log does not match project version file {VERSION_FILE}"
-
-
-def test_container_version_label_matches(version_container):
-    """Verify the container version label is the correct version."""
-    pkg_vars = {}
-    with open(VERSION_FILE) as f:
-        exec(f.read(), pkg_vars)  # nosec
-    project_version = pkg_vars["__version__"]
-    assert (
-        version_container.labels["version"] == project_version
-    ), "Dockerfile version label does not match project version"
->>>>>>> 4de6b59dd041229073cb15571ec5a1c005f6cad6
