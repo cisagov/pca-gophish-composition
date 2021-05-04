ca7b5add201027d48ff724e34b482860ed2f49cd
Nick Viola <nick.viola@associates.cisa.dhs.gov>
2021-05-04 17:29:30 -0500

    cleanup and fixing CI failures

--- gophish-tools/lipca-setup.sh
+++ gophish-tools/lipca-setup.sh
@@ -3,9 +3,7 @@
 
 TEMP_DIR_NAME=lipca-temp
 FULL_TEMP_PATH=$('pwd')/$TEMP_DIR_NAME
-LOG_FILE="$FULL_TEMP_PATH/log-`date +'%m-%d-%Y_%H-%M-%S'`"
-SERVER_API_KEY=some-key
-GOPHISH_HOST=localhost
+LOG_FILE="$FULL_TEMP_PATH/log-$(date +'%m-%d-%Y_%H-%M-%S')"
 PCA_GOPHISH_HELPER_DIR=/var/pca/pca-gophish-composition/gophish-tools
 
 
@@ -14,12 +12,11 @@ PCA_GOPHISH_HELPER_DIR=/var/pca/pca-gophish-composition/gophish-tools
 ######################
 
 create_assessment () {
-  WIZARD_OUTPUT_PATH=/var/cisagov/gophish-tools/assessment/builder.py
-  ASSESSMENT_NAME="assessment-`date +'%m-%d-%Y_%H-%M-%S'`"
+  ASSESSMENT_NAME="assessment-$(date +'%m-%d-%Y_%H-%M-%S')"
   FULL_ASSESSMENT_PATH=$TEMP_FULL_PATH/$ASSESSMENT_NAME
 
   # Run using docker gophish-tools image
-  sudo docker run -it -v $('pwd')/$TEMP_DIR_NAME:/home/cisa cisagov/gophish-tools pca-wizard $ASSESSMENT_NAME
+  sudo docker run -it -v $('pwd')/$TEMP_DIR_NAME:/home/cisa cisagov/gophish-tools pca-wizard "$ASSESSMENT_NAME"
   echo "Saved Assessment at: $TEMP_FULL_PATH/$ASSESSMENT_NAME"
 }
 
@@ -29,13 +26,14 @@ create_assessment () {
 #####################
 
 import_assessment () {
-  # Run locally pca-gophish-composition/gophish-tools 
-  cd $PCA_GOPHISH_HELPER_DIR && ./import_assessment.sh $FULL_ASSESSMENT_PATH
+  # Run locally pca-gophish-composition/gophish-tools
+  cd $PCA_GOPHISH_HELPER_DIR && ./import_assessment.sh "$FULL_ASSESSMENT_PATH"
 
   # Run using docker gophish-tools image  TODO: Determine if needed
   # IMPORT_SCRIPT_DOCKER_PATH=/app/pca-gophish-composition/gophish-tools/import_assessment.sh
-  # docker run -it -v $('pwd')/$TEMP_DIR_NAME:/home/cisa cisagov/gophish-tools gophish-import $ASSESSMENT_NAME.json $SERVER_API_KEY $GOPHISH_HOST
+  # docker run -it -v $('pwd')/$TEMP_DIR_NAME:/home/cisa cisagov/gophish-tools gophish-import $ASSESSMENT_NAME.json
 }
+
 
 
 #####################
@@ -43,8 +41,9 @@ import_assessment () {
 #####################
 
 complete_campaign () {
-  # Run locally pca-gophish-composition/gophish-tools 
-  cd $PCA_GOPHISH_HELPER_DIR && ./complete_campaign.sh $CAMPAIGN_ID
+  # Run locally pca-gophish-composition/gophish-tools
+  cd $PCA_GOPHISH_HELPER_DIR && ./complete_campaign.sh "$CAMPAIGN_ID"
+  99
 
   # Run using docker gophish-tools image  TODO: Determine if needed
   # COMPLETE_SCRIPT_DOCKER_PATH=/app/pca-gophish-composition/gophish-tools/complete_campaign.sh
@@ -84,7 +83,6 @@ handle_error () {
   create_assessment && import_assessment
 
   echo "Li_PCA Setup Complete."
-} || {
+  } || {
   handle_error
 }
-
