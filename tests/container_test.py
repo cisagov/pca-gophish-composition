c631691027f9d7cd847545d7e90ac41e4155b237
Jeremy Frasier <jeremy.frasier@gwe.cisa.dhs.gov>
2026-05-12 15:28:31 -0400

    Ensure that all function variables are lower case
    This change gets rid of several N806 errors from our flake8 pre-commit
    linter.

--- tests/container_test.py
+++ tests/container_test.py
@@ -20,59 +20,59 @@ def test_container_count(dockerc):
 
 def test_successful_exit_gophish_tools(gophish_tools_container):
     """Confirm successful exit code from gophish-tools container."""
-    SUCCESSFUL_EXIT = 0
-    TIMEOUT = 10
-    for i in range(TIMEOUT):
-        if gophish_tools_container.state.exit_code == SUCCESSFUL_EXIT:
+    successful_exit = 0
+    timeout = 10
+    for i in range(timeout):
+        if gophish_tools_container.state.exit_code == successful_exit:
             break
         time.sleep(1)
     else:
         raise Exception(
             f"Container did not exit successfully.  "
-            f'Expected exit code "{SUCCESSFUL_EXIT}" within {TIMEOUT} seconds.'
+            f'Expected exit code "{successful_exit}" within {timeout} seconds.'
         )
 
 
 def test_wait_for_ready_gophish(gophish_container):
     """Wait for gophish container to be ready."""
-    TIMEOUT = 10
+    timeout = 10
     ready_message = READY_MESSAGES["gophish"]
-    for i in range(TIMEOUT):
+    for i in range(timeout):
         if ready_message in gophish_container.logs():
             break
         time.sleep(1)
     else:
         raise Exception(
             f"Container does not seem ready.  "
-            f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
+            f'Expected "{ready_message}" in the log within {timeout} seconds.'
         )
 
 
 def test_wait_for_ready_postfix(postfix_container):
     """Wait for postfix container to be ready."""
-    TIMEOUT = 10
+    timeout = 10
     ready_message = READY_MESSAGES["postfix"]
-    for i in range(TIMEOUT):
+    for i in range(timeout):
         if ready_message in postfix_container.logs():
             break
         time.sleep(1)
     else:
         raise Exception(
             f"Container does not seem ready.  "
-            f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
+            f'Expected "{ready_message}" in the log within {timeout} seconds.'
         )
 
 
 def test_wait_for_ready_mailhog(mailhog_container):
     """Wait for mailhog container to be ready."""
-    TIMEOUT = 10
+    timeout = 10
     ready_message = READY_MESSAGES["mailhog"]
-    for i in range(TIMEOUT):
+    for i in range(timeout):
         if ready_message in mailhog_container.logs():
             break
         time.sleep(1)
     else:
         raise Exception(
             f"Container does not seem ready.  "
-            f'Expected "{ready_message}" in the log within {TIMEOUT} seconds.'
+            f'Expected "{ready_message}" in the log within {timeout} seconds.'
         )
